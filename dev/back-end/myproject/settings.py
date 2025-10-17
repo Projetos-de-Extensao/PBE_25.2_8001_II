64a16d5182f15e3846eaf16fe6d976ad27f711ab
Cauan_Baptista <cauanbaptista123@gmail.com>
2025-10-17 09:10:13 -0300

    Adiciona estrutura inicial do projeto Django com configurações, modelos, views e rotas

--- dev/back-end/myproject/settings.py
+++ dev/back-end/myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h6i@6e(+j7v$j)amlb*2*0+f2!9+un1hg_rtjmu6bt7%48k35b'
+SECRET_KEY = 'django-insecure-xxmeq-do^e2hmwo@5@0kc7i6hxwuj6*&@f#s^4^&z^ou%numc!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',  # Django REST Framework
-    'django_filters',  # Suporte a filtros
-    'core',  # Nossa aplicação
+    'myapp'
 ]
 
 MIDDLEWARE = [
@@ -123,14 +121,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Django REST Framework settings
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
-}
